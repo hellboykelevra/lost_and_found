782394843f9bd8152aa0f2ad50f3044b69971c90
hellboykelevra <silver.correo@gmail.com>
2025-01-09 13:33:27 +0000

    send_info has new functions for DB manage

--- send_info.php
+++ send_info.php
@@ -1,33 +1,24 @@
 <?php 
-  
-  $server = "localhost";
-  $user = "root";
-  $psswrd = "";
-  $DB = "lostAndFoundDB"; 
-
-  connect_DB($server, $user, $psswrd, $DB);
 
   if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['send']) ) {
-      // Recoger el nombre y los comentarios
-      $name = htmlspecialchars($_POST['caption']);
+      
+      $caption = htmlspecialchars($_POST['caption']);
       $initial_location = htmlspecialchars($_POST['initial_location']);
       $final_location = htmlspecialchars($_POST['final_location']);
       $date = htmlspecialchars($_POST['date']);
   
-      // Manejar la subida de la imagen
+      
       if (isset($_FILES['picture']) && $_FILES['picture']['error'] === 0) {
           $directorioSubida = 'uploads/';
-          $nombreArchivo = basename($_FILES['picture']['name']);
-          $rutaCompleta = $directorioSubida . $nombreArchivo;
+          $fileName = basename($_FILES['picture']['name']);
+          $path = $directorioSubida . $fileName;
   
-          // Crear directorio si no existe
           if (!is_dir($directorioSubida)) {
               mkdir($directorioSubida, 0755, true);
           }
   
-          // Mover archivo a la carpeta de destino
-          if (move_uploaded_file($_FILES['picture']['tmp_name'], $rutaCompleta)) {
-                save_info($img, $caption, $initial_location, $final_location, $date, $rutaCompleta, $nombreArchivo);
+          if (move_uploaded_file($_FILES['picture']['tmp_name'], $path)) {
+                save_info($img, $caption, $initial_location, $final_location, $date, $path, $fileName);
           } else {
               echo "Hubo un problema al subir la imagen.";
           }
@@ -39,19 +30,32 @@
       header("Location: index.php", true, 302); exit;
   }
 
-  function save_info($img, $caption, $initial_location, $final_location, $date, $rutaCompleta, $nombreArchivo){
-    echo "¡Formulario enviado con éxito!<br>";
-    echo "Nombre: " . $name . "<br>";
-    echo "Ubicación inicial: " . $initial_location . "<br>";
-    echo "Ubicación final: " . $final_location . "<br>";
-    echo "Fecha en que se encontró: " . $date . "<br>";
-    echo "Imagen subida correctamente: <a href='$rutaCompleta'>$nombreArchivo</a>";
-  }
+  function save_info($caption, $initial_location, $final_location, $date, $path, $fileName){
+    
+    $server = "localhost";
+    $user = "root";
+    $psswrd = "";
+    $DB = "lostAndFoundDB"; 
+    $conn = connect_DB($server, $user, $psswrd, $DB);
+    
+    if ($conn != NULL){
+        echo "¡Formulario enviado con éxito!<br>";
+        echo "Nombre: " . $caption . "<br>";
+        echo "Ubicación inicial: " . $initial_location . "<br>";
+        echo "Ubicación final: " . $final_location . "<br>";
+        echo "Fecha en que se encontró: " . $date . "<br>";
+        echo "Imagen subida correctamente: <a href='$$path'>$fileName</a>";
+  
+    close_connection($conn);
+    }
+    
+}
 
   function connect_DB($server, $user, $psswrd, $DB){
     $conn = new mysqli($server, $user, $psswrd, $DB);
 
     if ($conn->connect_error) {
+        return null;
         die("Error en la conexión: " . $conn->connect_error);
     }
     else{
@@ -64,4 +68,8 @@
     $conn->close();
   }
 
+  function create_insert_query ($caption, $initial_location, $final_location, $date, $path, $fileName){
+    $query = "INSERT INTO lost_object (caption, initial_location, final_location, found, found_date, img_path, file_name) VALUES ('[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]'
+       );";
+  }
   ?>
